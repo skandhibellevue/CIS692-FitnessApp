0b2eff0a110564f421fa4fcdf74092ecd4913718
Swetha Kandhi <skandhi@my365.bellevue.edu>
2025-01-27 16:32:54 -0600

    added edit and delete buttons to weight in history screen
    Signed-off-by: Srujan Simha Adicharla <srujansimha.adicharla@7-11.com>

--- app/src/main/java/com/example/fitnessapp/screens/HistoryScreen.kt
+++ app/src/main/java/com/example/fitnessapp/screens/HistoryScreen.kt
@@ -2,16 +2,29 @@ package com.example.fitnessapp.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -92,45 +106,133 @@ fun HistoryScreen(navController: NavHostController) {
                 modifier = Modifier.align(Alignment.Start)
             )
 
-            Box(
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .background(SmokeGray, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
             ) {
-                Text(
-                    text = selectedWeight, // Display the selected weight
-                    style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .background(SmokeGray, RoundedCornerShape(8.dp))
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = selectedWeight, // Display the selected weight
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Spacer(modifier = Modifier.width(20.dp))
 
+                if (selectedWeight != "No data") {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(40.dp)
+                    ) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        IconButton(
+                            onClick = { },
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                                .background(color = SmokeGray)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "Edit Icon",
+                                tint = Color.Black
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        IconButton(
+                            onClick = { },
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                                .background(color = Color.Red)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete Icon",
+                                tint = Color.White
+                            )
+                        }
+                    }
+                }
+            }
             Spacer(modifier = Modifier.height(32.dp))
 
-            // Notes Display
-            Text(
-                text = "Notes",
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.align(Alignment.Start)
-            )
-
-            Box(
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Top,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(120.dp)
-                    .padding(top = 8.dp)
-                    .background(SmokeGray, RoundedCornerShape(8.dp))
-                    .padding(16.dp)
+                    .height(IntrinsicSize.Min)
             ) {
-                Text(
-                    text = selectedNotes,
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
-                        .verticalScroll(rememberScrollState()),
-                    style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.Bold
-                )
+                        .weight(1f)
+                        .height(IntrinsicSize.Min)
+                ) {
+                    // Notes Display
+                    Text(
+                        text = "Notes",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(120.dp)
+                            .padding(top = 8.dp)
+                            .background(SmokeGray, RoundedCornerShape(8.dp))
+                            .padding(16.dp)
+                    ) {
+                        Text(
+                            text = selectedNotes,
+                            modifier = Modifier
+                                .verticalScroll(rememberScrollState()),
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.width(20.dp))
+
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(IntrinsicSize.Min)
+                ) {
+                    // Notes Display
+                    Text(
+                        text = "Progress Picture",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Box(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(120.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Arrow Down Icon",
+                            modifier = Modifier.size(150.dp),
+                            tint = Color.Gray
+                        )
+                    }
+                }
             }
         }
     }
